15f5fa31e6fa16452b61bd7317fd70b8a2a889bc
TegHocine <arcou98@hotmail.com>
2022-03-18 13:51:10 +0100

    chore: file change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from './components/Board'
+import KeyBoard from './components/KeyBoard'
+
+import AppContext from './context/AppContext'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Wordle Clone</h1>
+      <AppContext>
+        <Board />
+        <KeyBoard />
+      </AppContext>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
